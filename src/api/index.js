3e347216e84ab04cab02cc2618e2f9a36ba92980
Asileka <asilkatya@gmail.com>
2023-01-04 18:12:57 +0000

    changed setAreas to areaData

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const getAreaData = async () => {
   const { data } = await axios.get("https://api.zippopotam.us/GB/bb10");
-
+  // console.log(data.places);
   return data.places;
 };
