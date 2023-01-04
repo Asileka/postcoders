4ee93714a3cc8d1cd10d61510ab05dff08e8fbfe
Asileka <asilkatya@gmail.com>
2023-01-04 19:14:35 +0000

    added input for user to change postcode

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import { getAreaData } from "./api/index.js";
 import "./App.css";
 
 function App() {
+  const [postcode, setPostcode] = useState("BB10");
+  const [postcodeText, setPostcodeText] = useState("BB10");
   const [areas, setAreas] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setPostcodeText(postcode);
+  }
 
   const load = async () => {
     try {
-      const areaData = await getAreaData();
+      const areaData = await getAreaData(postcode);
 
       //areas.concat(areaData);
 
@@ -20,12 +27,23 @@ function App() {
 
   useEffect(() => {
     load();
-  }, []);
+  }, [postcodeText]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <h2>{`Areas for ${postcodeText}: ${areas.length}`}</h2>
+      <form>
+        <label>
+          Enter outcode:
+          <input
+            onChange={(e) => setPostcode(e.target.value)}
+            // defaultValue={postcode}
+            //placeholder="BB10"
+          />
+        </label>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
